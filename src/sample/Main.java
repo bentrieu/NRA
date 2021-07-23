339d5eb67dc43fa896c82f79501cff0f3d4daf43
Bacenz <s3878466@rmit.edu.vn>
2021-07-23 17:22:25 +0700

    Revert "Update Main.java"
    This reverts commit 7f3bfc6c92830dd40387cefb4df72b74bb51e59d.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,17 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("UI_Sample");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
-        System.out.println("push");
     }
 }
