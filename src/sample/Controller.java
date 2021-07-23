646247ec6cf1ce2d497f63d592c337bc1b5527cc
Bacenz <s3878466@rmit.edu.vn>
2021-07-23 17:22:28 +0700

    Revert "Update Controller.java"
    This reverts commit f2d5af3abb2f8dc6434016a73726e89d2b95936c.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,38 +1,4 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-import java.awt.*;
-
-
 public class Controller {
-
-    private Stage stage;
-    private Scene scene;
-    @FXML
-    private Pane pnl_covid,pnl_new,pnl_business,temp;
-    @FXML
-    private Button bt_covid,bt_new,bt_business;
-    @FXML
-    public void buttonAction (ActionEvent event)
-    {
-        if(event.getSource()==bt_covid)
-        {
-            pnl_covid.toFront();
-        }
-        else if(event.getSource()==bt_new)
-        {
-            pnl_new.toFront();
-        }
-        else if(event.getSource()==bt_business)
-        {
-            pnl_business.toFront();
-        }
-    }
-
 }
