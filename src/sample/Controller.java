6c041511df50fc8273a0f325d1f0dff9475d3336
ngongocthinh159 <s3879364@rmit.edu.vn>
2021-07-26 03:37:59 +0700

    Update loading animation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,23 +1,36 @@
 package sample;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
     @FXML
     private Button closeButton;
     @FXML
     private Button gettingStartedButton;
+    @FXML
+    private Label loadingLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadingLabel.setVisible(false);
+    }
 
     public void close(ActionEvent event) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -26,15 +39,28 @@ public class Controller {
         alert.setContentText("Are you sure?");
 
         if (alert.showAndWait().get() == ButtonType.OK) {
-            ((Stage) ((Node)event.getSource()).getScene().getWindow()).close();
+            System.exit(0);
         }
 
     }
 
     public void gettingStarted(ActionEvent event) throws IOException {
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(500), gettingStartedButton);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        loadingLabel.setVisible(true);
+        FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(2000), loadingLabel);
+        fadeTransition1.setFromValue(0);
+        fadeTransition1.setToValue(1);
+        fadeTransition1.play();
+
+        /*// Translate current scene to home scene
         Parent nextRoot = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
         Scene scene = ((Node)event.getSource()).getScene();
-        scene.setRoot(nextRoot);
+        scene.setRoot(nextRoot);*/
     }
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,14 +1,20 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    double initialX, initialY;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -16,11 +22,34 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         primaryStage.setTitle("Hello World");
         primaryStage.initStyle(StageStyle.TRANSPARENT);
+        addDraggableNode(root);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    // Draggable method for root (layout)
+    private void addDraggableNode(final Node node) {
+        node.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                if (me.getButton() != MouseButton.MIDDLE) {
+                    initialX = me.getSceneX();
+                    initialY = me.getSceneY();
+                }
+            }
+        });
+
+        node.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                if (me.getButton() != MouseButton.MIDDLE) {
+                    node.getScene().getWindow().setX(me.getScreenX() - initialX);
+                    node.getScene().getWindow().setY(me.getScreenY() - initialY);
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         launch(args);
