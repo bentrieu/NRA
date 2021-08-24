859a203f4dac99046044b6bdcf4da468dc7135ba
Bacenz <s3878466@rmit.edu.vn>
2021-08-24 17:36:07 +0700

    add articleList and article managere

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,39 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import java.io.IOException;
 
 public class Main extends Application {
-    public static Stage stage;
-
-    double initialX, initialY;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Stage stage = new Stage();
-        Main.stage = stage;
-
-        // Load the first news category
-        try {
-            ArticlesList.getNewsList();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Parent root = FXMLLoader.load(getClass().getResource("WelcomeScene.fxml"));
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        DraggableHelper.addDraggableNode(root);
-        scene.setFill(Color.TRANSPARENT);
-
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
