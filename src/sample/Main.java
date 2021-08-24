5795b335a3f95f7c85cc412aca321f848e533e23
quynguyn <86903423+quynguyn@users.noreply.github.com>
2021-08-24 13:02:27 +0700

    update main, HomeScene.fxml , DraggableHelper.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,48 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends Application {
+    public static Stage stage;
+
+    double initialX, initialY;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Stage stage = new Stage();
+        Main.stage = stage;
+
+        // Load the first news category
+//        try {
+//            ArticlesList.getNewsList();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        Parent root = FXMLLoader.load(getClass().getResource("WelcomeScene.fxml"));
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        DraggableHelper.addDraggableNode(root);
+        scene.setFill(Color.TRANSPARENT);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
